3024c418f7f00fd4a6efccb87ed357dfe7c88c5c
jsmnglng <jsmn.glng@gmail.com>
2019-03-21 15:48:03 -0700

    Initial Call to Action

--- src/components/LandingPage/Body.js
+++ src/components/LandingPage/Body.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import { Button } from 'grommet';
+import { FormNextLink } from 'grommet-icons';
 import styled from 'styled-components';
 
 const CallToAction = styled.h1`
-  font-size: 4rem;
+  padding-top: 90px;
+  h1 {
+    font-size: 7rem;
+    line-height: 8.5rem;
+    font-weight: 700;
+  }
 `;
 
 const Body = () => {
   return (
     <div>
       <CallToAction>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        <h1>Lorem ipsum dolor sit amet, consectetur elit</h1>
       </CallToAction>
+      <Button label="Get Started for Free" icon={<FormNextLink />} />
     </div>
   );
 };
