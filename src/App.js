1a7c47ea7a2c8f0207247931602e18135d7e30e0
sumi419 <sasgar04@gmail.com>
2019-03-21 09:23:49 -0700

    🎀 added axios to fetch data from BE

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+axios.defaults.withCredentials = true;
 
 class App extends Component {
+  componentDidMount() {
+    const api = 'https://still-shynin-still-strugglin.herokuapp.com/';
+    axios
+      .get(api)
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => console.error(err));
+  }
   render() {
     return (
       <div className="App">
