4b681eb2086718d26569d7040c5f4b5f5462eb9f
jsmnglng <jsmn.glng@gmail.com>
2019-03-21 15:24:49 -0700

    Implemented LandingPage component

---
+++ src/components/LandingPage/Body.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const CallToAction = styled.h1`
+  font-size: 4rem;
+`;
+
+const Body = () => {
+  return (
+    <div>
+      <CallToAction>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+      </CallToAction>
+    </div>
+  );
+};
+
+export default Body;
