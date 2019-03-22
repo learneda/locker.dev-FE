9786c804aed537cb14c60ec481dbc58a8b2f1827
Riley-Brown <riley@riley.gg>
2019-03-22 15:31:11 -0400

    updates heroku production URLs

--- src/services/baseURL.js
+++ src/services/baseURL.js
@@ -1,4 +1,4 @@
 export const post =
   process.env.NODE_ENV === 'production'
-    ? 'https://still-shynin-still-strugglin.herokuapp.com'
+    ? 'https://learned-a.herokuapp.com'
     : 'http://localhost:8000';
