98bb0d479c7b6c1f64dea2d72fc9a4adfefa216e
Riley-Brown <riley@riley.gg>
2019-03-21 22:31:28 -0400

    Imported react router and wrapped App component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import App from './App';
 import rootReducer from './reducers';
@@ -11,7 +12,10 @@ const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
+
   document.getElementById('root')
 );
