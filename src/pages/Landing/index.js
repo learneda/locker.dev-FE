0933e47b638ecff510c8951e907d9980a362e87d
samsisle <samko9522@gmail.com>
2019-03-27 13:24:50 -0700

    🍾 Move LandingPage into pages directory (pages/Landing)

---
+++ src/pages/Landing/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Body from './Body';
+import { Wrapper } from '../../components/mixins';
+
+export default () => {
+  return (
+    <Wrapper>
+      <Body />
+    </Wrapper>
+  );
+};
