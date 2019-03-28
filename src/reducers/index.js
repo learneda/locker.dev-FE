d1cf91e3fc5c103c72c03299caee773e23d55781
samsisle <samko9522@gmail.com>
2019-03-28 15:24:42 -0700

    🍷 Update combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,12 @@
 import { combineReducers } from 'redux';
 import { authReducer } from './authReducer';
 import { browseReducer } from './browseReducer';
+import { modalReducer } from './modalReducer';
 import { postReducer } from './postReducer';
 
 export default combineReducers({
   auth: authReducer,
   browse: browseReducer,
-  modalState: authReducer,
+  modalState: modalReducer,
   post: postReducer
 });
