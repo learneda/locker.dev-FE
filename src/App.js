a42a041ad16d55dbe8c81d4171a9c074a7b7eed8
samsisle <samko9522@gmail.com>
2019-03-28 15:21:35 -0700

    🥔 Add fetchUser logic to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import LandingPage from './pages/Landing';
 import Navbar from './components/Navbar';
@@ -9,18 +10,35 @@ import Browse from './components/Browse';
 import NoMatch from './components/NoMatch';
 
 import { Container } from './components/mixins';
+import { fetchUser } from './actions';
 
-export default function App() {
-  return (
-    <Container>
-      <Navbar />
-      <Switch>
-        <Route component={LandingPage} exact path="/" />
-        <Route component={Homepage} path="/home" />
-        <Route component={Browse} path="/browse" />
-        <Route component={UserProfile} path="/profile" />
-        <Route component={NoMatch}/>
-      </Switch>
-    </Container>
-  );
+class App extends Component {
+  componentDidMount = () => this.props.fetchUser();
+  componentDidUpdate = prevProps => {
+    if (this.props.location !== prevProps.location) {
+      this.props.fetchUser();
+    }
+  };
+
+  render() {
+    return (
+      <Container>
+        <Navbar />
+        <Switch>
+          <Route component={LandingPage} exact path="/" />
+          <Route component={Homepage} path="/home" />
+          <Route component={Browse} path="/browse" />
+          <Route component={UserProfile} path="/profile" />
+          <Route component={NoMatch} />
+        </Switch>
+      </Container>
+    );
+  }
 }
+
+export default withRouter(
+  connect(
+    null,
+    { fetchUser }
+  )(App)
+);
