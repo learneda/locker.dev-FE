14e50247aa5dc7265392f1fe67655c543b83d714
luiscmartinez <mrluismartinezzz@gmail.com>
2019-03-28 14:53:25 -0700

    imported redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -9,6 +9,8 @@ import App from './App';
 import rootReducer from './reducers';
 import 'typeface-roboto';
 import './styles/index.css';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 
 let store;
 
@@ -16,7 +18,7 @@ if (process.env.NODE_ENV === 'production') {
   store = createStore(rootReducer, applyMiddleware(thunk));
 } else {
   const logger = require('redux-logger').default;
-  store = createStore(rootReducer, applyMiddleware(thunk, logger));
+  store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, logger)));
 }
 
 ReactDOM.render(
