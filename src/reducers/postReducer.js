f23b0f07f484b15ada5e3453f66554876d2db720
sumi419 <sasgar04@gmail.com>
2019-03-28 19:13:31 -0700

    removed save link

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,12 +1,10 @@
-import { SAVE_LINK, ERROR } from '../actions/types';
+import { ERROR } from '../actions/types';
 
 const initialState = {
   posts: []
 };
 export const postReducer = (state = initialState, action) => {
   switch (action.type) {
-    case SAVE_LINK:
-      return { ...state, posts: [...action.payload] };
     case ERROR:
       return { ...state, error: action.payload };
     default:
