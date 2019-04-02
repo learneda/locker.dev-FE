72ab0f552bff76c62363879a20dede53bda537dc
jsmnglng <jsmn.glng@gmail.com>
2019-04-02 16:17:31 -0700

    Added Tabs for Bookmarks and Likes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,30 @@ import React, { Component } from 'react';
 import Bookmarks from '../components/Bookmarks';
 import Likes from '../components/Likes';
 import Sidebar from '../components/Sidebar';
+import styled from 'styled-components';
+
+import { customWrapper } from '../components/mixins';
+import { Tab, Tabs } from 'grommet';
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
+      <Container>
         <Sidebar />
-        <Bookmarks />
-        <Likes />
-      </div>
+        <Tabs justify="start">
+          <Tab title="Bookmarks">
+            <Bookmarks />
+          </Tab>
+          <Tab title="Likes">
+            <Likes />
+          </Tab>
+        </Tabs>
+      </Container>
     );
   }
 }
+
+const Container = styled.div`
+  ${customWrapper('80%', '0 auto')}
+  display: flex;
+`;
