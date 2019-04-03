984914aeca5aa82c04eb543f243988dc53fac75e
samsisle <samko9522@gmail.com>
2019-04-02 23:01:22 -0700

    🌭 fix name of reducer to match import in reducers/index.js

--- src/reducers/likedPostReducer.js
+++ src/reducers/likedPostReducer.js
@@ -1,6 +1,6 @@
 import { LIKED_POSTS } from '../actions/types';
 
-export const likedPostsReducer = (state = [], action) => {
+export const likedPostReducer = (state = [], action) => {
   switch (action.type) {
     case LIKED_POSTS:
       return action.payload;
