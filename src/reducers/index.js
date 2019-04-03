a08eca1d6a0dfbdac95a8cf0826635a4fa749df4
samsisle <samko9522@gmail.com>
2019-04-02 22:50:21 -0700

    🍌 Update combineReducer with likedPost reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import { authReducer } from './authReducer';
 import { browseReducer } from './browseReducer';
 import { modalReducer } from './modalReducer';
 import { postReducer } from './postReducer';
+import { likedPostReducer } from './likedPostReducer';
 
 export default combineReducers({
   auth: authReducer,
   browse: browseReducer,
   modalState: modalReducer,
-  posts: postReducer
+  posts: postReducer,
+  likedPosts: likedPostReducer
 });
