76d7c94f9dbee9d0afb14524f111a992b028396d
luiscmartinez <mrluismartinezzz@gmail.com>
2019-04-04 13:38:48 -0700

    connected redux search state with input value

--- src/components/DummySearch.js
+++ src/components/DummySearch.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { TextInput } from 'grommet';
+import {getSearchValue} from '../actions';
+import {connect} from 'react-redux';
 
-export default function DummySearch() {
+function DummySearch(props) {
   const [value, setValue] = useState('');
   const handleChange = e => setValue(e.target.value);
 
@@ -10,9 +12,16 @@ export default function DummySearch() {
       <TextInput
         size="small"
         placeholder="search"
-        value={value}
-        onChange={handleChange}
+        value={props.search_term}
+        onChange={(e) => props.getSearchValue(e)}
       />
     </div>
   );
 }
+const mapStateToProps = (state) => {
+  return {
+    search_term: state.search_term
+  };
+};
+
+export default connect(mapStateToProps, {getSearchValue})(DummySearch);
