ddd7292f6fb06d22dc05416ede6396a44f85c05d
samsisle <samko9522@gmail.com>
2019-04-03 18:26:47 -0700

    🍅 Hardcode width styling to DummySearch component

--- src/components/DummySearch.js
+++ src/components/DummySearch.js
@@ -3,15 +3,16 @@ import { TextInput } from 'grommet';
 
 export default function DummySearch() {
   const [value, setValue] = useState('');
-
   const handleChange = e => setValue(e.target.value);
 
   return (
-    <TextInput
-      size="small"
-      placeholder="search"
-      value={value}
-      onChange={handleChange}
-    />
+    <div style={{ width: '200px' }}>
+      <TextInput
+        size="small"
+        placeholder="search"
+        value={value}
+        onChange={handleChange}
+      />
+    </div>
   );
 }
