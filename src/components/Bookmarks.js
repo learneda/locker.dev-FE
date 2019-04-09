af852f8bb331bcb5f389b8f2e0177fc2429c21e3
Riley-Brown <riley@riley.gg>
2019-04-09 17:38:33 -0400

    updated card width and height to be consistent

--- src/components/Bookmarks.js
+++ src/components/Bookmarks.js
@@ -176,8 +176,8 @@ const Post = styled.div`
   img {
     width: 100%;
     border-radius: 6px 0 0px 6px;
-    max-width: 320px;
-    max-height: 220px;
+    width: 320px;
+    height: 220px;
     object-fit: cover;
     height: 100%;
     @media (max-width: 1450px) {
