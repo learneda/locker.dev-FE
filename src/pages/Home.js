612a9affd09f1d04ceff07ef66cefa401c1a13bb
luiscmartinez <mrluismartinezzz@gmail.com>
2019-04-10 10:02:22 -0700

    removed all socket io code .....

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import openSocket from 'socket.io-client';
 import { customWrapper } from '../components/mixins';
 import { post as URL } from '../services/baseURL';
 import { connect } from 'react-redux';
@@ -18,59 +17,51 @@ class Home extends Component {
     console.log(props.auth.id)
     this.username = props.auth.username;
     this.user_id = props.auth.id;
-    this.socket = openSocket(URL);
   }
 
   componentDidMount () {
-    this.socket.on('comments', (msg) => {
-      console.log('here',msg)
-      this.setState({ comments: [ {username: msg.username, content:msg.msg.content}, ...this.state.comments ] })
-    })
     axios.get(`${URL}/api/users/newsfeed`).then((res) => {
       console.log('axios res', res.data);
       this.setState({posts:res.data.newResponse});
     }).catch((err) => console.log(err));
   }
 
-  handleChange = ({ target }) => {
-    const { name, value } = target;
+  handleChange = (e, post_id) => {
+    const { name, value } = e.target;
 
     this.setState({ [name]: value });
   };
 
-  handleSubmit = event => {
+  handleSubmit = (event, post_id) => {
     const body = event.target.value;
-
-    const comment = { action: 'create', content: body, user_id: this.user_id,
-    post_id: 10, username: this.username }
-
     if (event.keyCode === 13 && body) {
-      this.socket.emit('comments', comment)
-      this.setState({ comments: [ {username: this.username, content:body}, ...this.state.comments ] })
-
       event.target.value = '';
     }
   };
 
   render() {
-    // console.log(this.state.comments);
-    // const comments = this.state.comments.map((message, index) => {
-    //   console.log(message);
-    //   return (
-    //     <li key={index}>
-    //       {message.username} <SPAN>{message.content}</SPAN>
-    //     </li>
-    //   );
-    // });
     const posts = this.state.posts.map((post, index) => {
       return (
-        <div key={index}>
+        <div styles={{
+          border: '5px solid pink'
+        }} className='lol' key={index}>
           <div>{post.username}</div>
           <img src={`${post.profile_picture}`} alt='user_profile_pic' />
           <img src={`${post.thumbnail_url}`} alt='post_thumbnail' />
           <div>{post.title}</div>
           <div>{post.description}</div>
+
           {/* <div>{post.}</div> */}
+          <div>{post.comments.map((comment, index) => {
+            console.log(comment)
+            return (
+            <h1 className='comments'>{comment.content}</h1>
+          )})}
+          </div>
+          <input
+          placeholder='enter a comment...'
+          type='text'
+          onKeyUp={(e) => this.handleSubmit(e, post.post_id)}/>
         </div>
       );
     });
