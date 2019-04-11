0ebe09a0d262b2193c858ad5232c47a035c1b8c1
luiscmartinez <mrluismartinezzz@gmail.com>
2019-04-11 11:54:27 -0700

    newfeed comments work

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,11 +11,10 @@ class Home extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      comments: [],
       search: '',
       posts: []
     }
-    console.log(props.auth.id)
+    // console.log(props.auth.id)
     this.username = props.auth.username;
     this.user_id = props.auth.id;
     this.socket = openSocket(URL);
@@ -24,13 +23,24 @@ class Home extends Component {
   componentDidMount () {
     this.socket.on('comments', (msg) => {
       console.log('here',msg)
-      this.setState({ comments: [ {username: msg.username, content:msg.msg.content}, ...this.state.comments ] })
+      if (msg.action === 'create') {
+        const updated_state = this.state.posts.map((post, index) => {
+          if (post.post_id === msg.post_id) {
+            post.comments.push(msg);
+          }
+          return post
+        }); 
+        console.log('\n updated post',updated_state)
+        this.setState({posts: updated_state});
+      } 
     })
+
     axios.get(`${URL}/api/users/newsfeed`).then((res) => {
       console.log('axios res', res.data);
       this.setState({posts:res.data.newResponse});
     }).catch((err) => console.log(err));
   }
+
 
   handleChange = ({ target }) => {
     const { name, value } = target;
@@ -38,30 +48,21 @@ class Home extends Component {
     this.setState({ [name]: value });
   };
 
-  handleSubmit = event => {
+  handleSubmit = (event, post_id) => {
     const body = event.target.value;
 
     const comment = { action: 'create', content: body, user_id: this.user_id,
-    post_id: 10, username: this.username }
+    post_id: post_id, username: this.username }
 
     if (event.keyCode === 13 && body) {
       this.socket.emit('comments', comment)
-      this.setState({ comments: [ {username: this.username, content:body}, ...this.state.comments ] })
+      // this.setState({ comments: [ {username: this.username, content:body}, ...this.state.comments ] })
 
       event.target.value = '';
     }
   };
 
   render() {
-    // console.log(this.state.comments);
-    // const comments = this.state.comments.map((message, index) => {
-    //   console.log(message);
-    //   return (
-    //     <li key={index}>
-    //       {message.username} <SPAN>{message.content}</SPAN>
-    //     </li>
-    //   );
-    // });
     const posts = this.state.posts.map((post, index) => {
       return (
         <div key={index}>
@@ -70,7 +71,15 @@ class Home extends Component {
           <img src={`${post.thumbnail_url}`} alt='post_thumbnail' />
           <div>{post.title}</div>
           <div>{post.description}</div>
-          {/* <div>{post.}</div> */}
+          <div>{post.comments.map((comment, index) => {
+]            return (
+            <h1 key={index}className='comments'>{comment.username} {comment.content}</h1>
+          )})}
+          </div>
+          <input
+          placeholder='enter a comment...'
+          type='text'
+          onKeyUp={(e) => this.handleSubmit(e, post.post_id)}/>
         </div>
       );
     });
