cf2c0ba7311760f01dc006e8ded05a3545f36285
Riley-Brown <riley@riley.gg>
2019-04-11 19:16:52 -0400

    fixed image profile image sizing

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -97,7 +97,8 @@ const Profile = styled.div`
       top: 30px;
       border: 3px solid #fff;
       border-radius: 50%;
-      height: auto;
+      height: 100px;
+      object-fit: cover;
       width: 100px;
     }
   }
