cb161a4c6ac5740d47aea6f7f71b10660e53fb69
Riley-Brown <riley@riley.gg>
2019-04-10 21:44:01 -0400

    renameed UserProfile page to AuthUserProfile

---
+++ src/pages/AuthUserProfile.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import Bookmarks from '../components/Bookmarks';
+import Likes from '../components/Likes';
+import Sidebar from '../components/Sidebar';
+import styled from 'styled-components';
+
+import { customWrapper } from '../components/mixins';
+import { Grommet, Tab, Tabs } from 'grommet';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <Container>
+        <Sidebar />
+        <Wrapper>
+          <Grommet theme={theme}>
+            <Tabs justify="start" className="tabs">
+              <Tab title="Bookmarks">
+                <TabWrapper>
+                  <Bookmarks />
+                </TabWrapper>
+              </Tab>
+              <Tab title="Recommended">
+                <TabWrapper>
+                  <Likes />
+                </TabWrapper>
+              </Tab>
+            </Tabs>
+          </Grommet>
+        </Wrapper>
+      </Container>
+    );
+  }
+}
+
+const theme = {
+  tab: {
+    color: 'dark-1',
+    active: {
+      weight: 'bold'
+    },
+    border: {
+      side: 'bottom',
+      size: 'medium',
+      color: {
+        light: null
+      },
+      active: {
+        color: {
+          light: 'dark-1'
+        }
+      },
+      hover: {
+        color: {
+          light: null
+        }
+      },
+      margin: {
+        bottom: '30px'
+      }
+    }
+  }
+};
+
+const Container = styled.div`
+  ${customWrapper('80%', '0 auto')}
+  display: flex;
+`;
+
+const Wrapper = styled.div`
+  ${customWrapper('75%')}
+  padding-left: 2%;
+  @media (max-width: 900px) {
+    width: 100%;
+  }
+`;
+
+const TabWrapper = styled.div`
+  border-top: 1px solid #bdbdbd;
+  padding-top: 20px;
+  margin-top: -3px;
+  margin-left: 12px;
+`;
