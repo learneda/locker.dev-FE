33b1c90abecbdf77baf6321bbdf14fc1e9dfb348
Riley-Brown <riley@riley.gg>
2019-04-12 18:41:11 -0400

    Added styling for Home/Newsfeed component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import { customWrapper } from '../components/mixins';
 import { post as URL } from '../services/baseURL';
 import { connect } from 'react-redux';
 import axios from 'axios';
+import Moment from 'react-moment';
 
 class Home extends Component {
   constructor(props) {
@@ -68,26 +69,46 @@ class Home extends Component {
   render() {
     const posts = this.state.posts.map((post, index) => {
       return (
-        <div key={index}>
-          <div>{post.username}</div>
-          <img src={`${post.profile_picture}`} alt="user_profile_pic" />
-          <img src={`${post.thumbnail_url}`} alt="post_thumbnail" />
-          <div>{post.title}</div>
-          <div>{post.description}</div>
-          <div>
+        <div key={index} className="post">
+          <div className="post-user-info">
+            <img src={`${post.profile_picture}`} alt="user_profile_pic" />
+            <div>
+              <h2>{post.username}</h2>
+              <Moment className="post-date" fromNow>
+                {post.created_at}
+              </Moment>
+            </div>
+            <div>
+              <hr />
+            </div>
+          </div>
+          <div className="post-content">
+            {post.thumbnail_url ? (
+              <img src={`${post.thumbnail_url}`} alt="post_thumbnail" />
+            ) : null}
+            <div className="title-and-description">
+              <h2>{post.title}</h2>
+              <p>{post.description}</p>
+            </div>
+          </div>
+          <div className="comments-container">
+            <div className="add-comment">
+              <img src={this.props.auth.profile_picture} alt="" />
+              <textarea
+                placeholder="Enter a comment..."
+                type="text"
+                onKeyUp={e => this.handleSubmit(e, post.post_id)}
+              />
+            </div>
             {post.comments.map((comment, index) => {
               return (
-                <h1 key={index} className="comments">
-                  {comment.username} {comment.content}
-                </h1>
+                <div key={index} className="comments">
+                  <h2>{comment.username}:</h2>
+                  <span>{comment.content}</span>
+                </div>
               );
             })}
           </div>
-          <input
-            placeholder="enter a comment..."
-            type="text"
-            onKeyUp={e => this.handleSubmit(e, post.post_id)}
-          />
         </div>
       );
     });
@@ -97,6 +118,99 @@ class Home extends Component {
 
 const Container = styled.div`
   ${customWrapper('80%', '0 auto')}
+  .post {
+    margin: auto;
+    margin-bottom: 40px;
+    border: 1px solid lightgray;
+    border-radius: 8px;
+    max-width: 700px;
+    background: #fff;
+  }
+  .post-user-info {
+    display: flex;
+    align-items: center;
+    padding: 15px 25px;
+    /* border: 1px solid red; */
+    border-bottom: 1px solid lightgray;
+    img {
+      width: 60px;
+      height: 60px;
+      border-radius: 50%;
+      margin-right: 10px;
+    }
+    h2 {
+      font-weight: 500;
+      margin-bottom: 3px;
+    }
+    .post-date {
+      font-size: 1.3rem;
+      opacity: 0.8;
+    }
+  }
+  .post-content {
+    display: flex;
+    flex-direction: column;
+    border-bottom: 1px solid lightgrey;
+    img {
+      max-height: 500px;
+      width: 100%;
+      height: 100%;
+    }
+  }
+  .title-and-description {
+    padding: 15px 25px;
+    h2 {
+      margin-bottom: 10px;
+      font-size: 2.6rem;
+      font-weight: 500;
+      line-height: 1.3;
+    }
+    p {
+      opacity: 0.8;
+      line-height: 1.6;
+    }
+  }
+  .comments-container {
+    padding: 15px 25px;
+    .add-comment {
+      display: flex;
+      align-items: center;
+      margin-bottom: 20px;
+      img {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        margin-right: 10px;
+      }
+      textarea {
+        resize: none;
+        align-items: center;
+        padding: 5px;
+        width: 100%;
+        ::placeholder {
+          /* border-bottom: 1px solid lightgrey; */
+        }
+        :focus {
+          border: 1px solid #3f65f2;
+          outline: none;
+        }
+      }
+    }
+    .comments {
+      display: flex;
+      margin-bottom: 10px;
+      h2 {
+        margin-right: 10px;
+        color: #222;
+        font-weight: 400;
+      }
+      span {
+        color: #222;
+        opacity: 0.9;
+        word-break: break-all;
+      }
+    }
+  }
 `;
 
 const SPAN = styled.span`
