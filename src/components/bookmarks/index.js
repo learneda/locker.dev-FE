dd0191d09ebe08e5b8755a1b5312a66c2b730d41
Riley-Brown <riley@riley.gg>
2019-04-23 21:17:41 -0400

    Refactored bookmarks component to use hooks

--- src/components/bookmarks/index.js
+++ src/components/bookmarks/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import Moment from 'react-moment';
 import axios from 'axios';
@@ -21,108 +21,96 @@ import editSvg from '../../assets/svg/edit.svg';
 import HelpScreen from '../utils/screens/HelpScreen';
 import BookmarkSVG from '../../assets/svg/bookmark-drawing.svg';
 
-class Bookmarks extends Component {
-  state = { modalOpen: false };
+const Bookmarks = props => {
+  const [modalOpen, setModalOpen] = useState(false);
 
-  componentDidMount = () => {
-    this.props.getPosts();
-  };
+  useEffect(() => {
+    props.getPosts();
+  }, []);
 
-  handleLike = async (id, liked) => {
+  const handleLike = async (id, liked) => {
     await axios.put(`${URL}/api/posts/like/${id}`, { status: !liked });
-    this.props.getPosts();
+    props.getPosts();
   };
 
-  handleTruncateText = (content, limit = 10) => truncateText(content, limit);
+  const handleTruncateText = (content, limit = 10) =>
+    truncateText(content, limit);
 
-  handleModalOpen = () => {
-    this.setState({
-      modalOpen: !this.state.modalOpen
-    });
+  const handleModalOpen = () => {
+    setModalOpen(!modalOpen);
   };
 
-  render() {
-    const search = this.props.search_term;
-    const filteredPosts = this.props.posts.filter(post => {
-      return post.title
-        ? post.title.toLowerCase().indexOf(search.toLowerCase()) !== -1
-        : null || post.thumbnail_url
-        ? post.thumbnail_url.toLowerCase().indexOf(search.toLowerCase()) !== -1
-        : null || post.description
-        ? post.description.toLowerCase().indexOf(search.toLowerCase()) !== -1
-        : null;
-    });
+  const search = props.search_term;
+  const filteredPosts = props.posts.filter(post => {
+    return post.title
+      ? post.title.toLowerCase().indexOf(search.toLowerCase()) !== -1
+      : null || post.thumbnail_url
+      ? post.thumbnail_url.toLowerCase().indexOf(search.toLowerCase()) !== -1
+      : null || post.description
+      ? post.description.toLowerCase().indexOf(search.toLowerCase()) !== -1
+      : null;
+  });
 
-    const posts = filteredPosts
-      .map(post => (
-        <Post key={post.id}>
+  const posts = filteredPosts
+    .map(post => (
+      <Post key={post.id}>
+        <a href={post.post_url} target="_blank" rel="noopener noreferrer">
+          <img src={post.thumbnail_url} alt="" />
+        </a>
+        <div className="post-content">
           <a href={post.post_url} target="_blank" rel="noopener noreferrer">
-            <img src={post.thumbnail_url} alt="" />
+            <h1>{handleTruncateText(post.title, 9)}</h1>
           </a>
-          <div className="post-content">
-            <a href={post.post_url} target="_blank" rel="noopener noreferrer">
-              <h1>{this.handleTruncateText(post.title, 9)}</h1>
-            </a>
-            <p>{this.handleTruncateText(post.description, 15)}</p>
-            <div className="date-like-heart">
-              <span className="formatted-date">
-                Added <Moment fromNow>{post.created_at}</Moment>
-              </span>
-              <Like
-                liked={post.liked}
-                handleLike={this.handleLike}
-                id={post.id}
-              />
-              <span className="rec-span">like</span>
-              <img
-                src={deleteIcon}
-                className="delete-icon"
-                onClick={async () =>
-                  await this.props
-                    .deletePost(post.id)
-                    .then(res => this.props.getPosts())
-                }
-                alt="delete icon"
-              />
-              <span className="del-span">delete</span>
-            </div>
+          <p>{handleTruncateText(post.description, 15)}</p>
+          <div className="date-like-heart">
+            <span className="formatted-date">
+              Added <Moment fromNow>{post.created_at}</Moment>
+            </span>
+            <Like liked={post.liked} handleLike={handleLike} id={post.id} />
+            <span className="rec-span">like</span>
+            <img
+              src={deleteIcon}
+              className="delete-icon"
+              onClick={async () =>
+                await props.deletePost(post.id).then(res => props.getPosts())
+              }
+              alt="delete icon"
+            />
+            <span className="del-span">delete</span>
           </div>
-          <img
-            src={editSvg}
-            alt=""
-            onClick={() => {
-              localStorage.setItem('editPostId', post.id);
-
-              this.setState({
-                modalOpen: !this.state.modalOpen
-              });
-            }}
-            className="edit-icon"
-          />
-        </Post>
-      ))
-      .reverse();
-
-    if (posts.length === 0) {
-      return (
-        <HelpScreen
-          imgSource={BookmarkSVG}
-          headerText="Your saved courses and articles will be stored here."
+        </div>
+        <img
+          src={editSvg}
+          alt=""
+          onClick={() => {
+            localStorage.setItem('editPostId', post.id);
+            handleModalOpen();
+          }}
+          className="edit-icon"
         />
-      );
-    }
+      </Post>
+    ))
+    .reverse();
+
+  if (posts.length === 0) {
     return (
-      <Wrapper>
-        <EditModal
-          key={localStorage.getItem('editPostId')}
-          open={this.state.modalOpen}
-          handleModalOpen={this.handleModalOpen}
-        />
-        {posts}
-      </Wrapper>
+      <HelpScreen
+        imgSource={BookmarkSVG}
+        headerText="Your saved courses and articles will be stored here."
+      />
     );
   }
-}
+  return (
+    <Wrapper>
+      <EditModal
+        key={localStorage.getItem('editPostId')}
+        open={modalOpen}
+        handleModalOpen={handleModalOpen}
+      />
+      {posts}
+    </Wrapper>
+  );
+};
 
 export const Wrapper = styled.div`
   // border: 1px solid blue;
