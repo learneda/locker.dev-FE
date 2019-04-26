1df6c7a5c08b8a3bb4adbbc4ed2e88eccc042aa2
cesarnml <cmejia@gmail.com>
2019-04-26 06:25:20 -0700

    minor typo

--- src/index.js
+++ src/index.js
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { positions, Provider as AlertProvider } from 'react-alert';
-import * as serviceWorker from './serviceWorker';
-import Alert from './components/utils/Alert';
-
-import App from './App';
-import rootReducer from './reducers';
-import 'typeface-roboto';
-import './styles/index.css';
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-let store;
-
-if (process.env.NODE_ENV === 'production') {
-  store = createStore(rootReducer, applyMiddleware(thunk));
-} else {
-  // const logger = require('redux-logger').default
-  store = createStore(
-    rootReducer,
-    // composeEnhancers(applyMiddleware(thunk, logger))
-    composeEnhancers(applyMiddleware(thunk))
-  );
-}
-=======
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
@@ -42,11 +12,10 @@ import * as serviceWorker from './serviceWorker'
 import 'typeface-roboto'
 import './styles/index.css'
 
->>>>>>> f1f2754cd1d8671588bd2f35540320d65ecd1a94
 const options = {
   position: positions.BOTTOM_CENTER,
   timeout: 3000,
-};
+}
 
 ReactDOM.render(
   <Provider store={store}>
@@ -57,5 +26,5 @@ ReactDOM.render(
     </Router>
   </Provider>,
   document.getElementById('root')
-);
-serviceWorker.unregister();
+)
+serviceWorker.unregister()
