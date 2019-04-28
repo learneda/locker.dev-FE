38836dc10cb86737b0c8f0dc7946228685e1f82c
luis <mrluismartinezzz@gmail.com>
2019-04-27 18:11:05 -0700

    removed non auth related styles from StyledAuth.js created index css for non auth related styles

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ import * as serviceWorker from './serviceWorker'
 
 import 'typeface-roboto'
 
+import '/Users/StarTask/lambda/labs/labs11_learned_a-FE/src/styles/index.css'
+
 const options = {
   position: positions.BOTTOM_CENTER,
   timeout: 3000,
