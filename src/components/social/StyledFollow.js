1f1197a0534fbb5363821c3b442b97e3a75b8868
Riley-Brown <riley@riley.gg>
2019-05-07 19:19:15 -0400

    Created file to handle reusable styles for social tab

---
+++ src/components/social/StyledFollow.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+export const StyledFollow = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  text-align: center;
+  &::after {
+    content: '';
+    flex: 0 0 29%;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    flex: 0 0 29%;
+    align-items: center;
+    background: #fff;
+    padding: 10px 15px;
+    border-radius: 8px;
+    margin-bottom: 30px;
+    justify-content: center;
+
+    img {
+      width: 100px;
+      height: 100px;
+      border-radius: 50%;
+      margin: 5px auto;
+    }
+    h2 {
+      opacity: 0.8;
+    }
+    button {
+      padding: 0;
+      background: #4064f2;
+      color: #fff;
+      border: none;
+      padding: 5px 10px;
+      font-weight: 700;
+      border-radius: 4px;
+      cursor: pointer;
+      font-size: 1.4rem;
+    }
+  }
+`;
