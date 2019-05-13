26d6ddb21d8ae61704b7cd3dbeed260e780f095f
Riley-Brown <riley@riley.gg>
2019-05-12 22:34:28 -0400

    added fresh memes

--- src/components/profile/UserFollowers.js
+++ src/components/profile/UserFollowers.js
@@ -4,6 +4,7 @@ export default function UserFollowers() {
   return (
     <div>
       <h1>hello</h1>
+      <p>I will fix this eventually.... maybe....</p>
     </div>
   );
 }
--- src/components/profile/UserFollowing.js
+++ src/components/profile/UserFollowing.js
@@ -4,6 +4,7 @@ export default function UserFollowing() {
   return (
     <div>
       <h1>hello</h1>
+      <p>I will fix this eventually....</p>
     </div>
   );
 }
