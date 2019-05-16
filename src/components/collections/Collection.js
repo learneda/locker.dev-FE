b772537619f92add5c0da7601af0058d59d329c8
luis <mrluismartinezzz@gmail.com>
2019-05-16 04:51:56 -0700

    moved some logic into Collection.js

---
+++ src/components/collections/Collection.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import moment from 'moment'
+import Moment from 'react-moment'
+import styled from 'styled-components'
+
+import EditModal from '../utils/EditModal/EditModal'
+import deleteIcon from '../../assets/svg/delete-icon.svg'
+import editSvg from '../../assets/svg/edit.svg'
+import SharedButton from '../shared'
+import { customWrapper } from '../mixins'
+import { StyledBookmarks } from './StyledBookmarks'
+
+export default function(props) {
+	const posts = props.posts
+		.map((post) => (
+			<Post key={post.id}>
+				<a href={post.post_url} target="_blank" rel="noopener noreferrer">
+					<img src={post.thumbnail_url} alt="" />
+				</a>
+				<div className="post-content">
+					<a href={post.post_url} target="_blank" rel="noopener noreferrer">
+						<h1>{props.handleTruncateText(post.title, 9)}</h1>
+					</a>
+					<p>{props.handleTruncateText(post.description, 15)}</p>
+					<a className="post-root-url" href={post.post_url} target="_blank" rel="noopener noreferrer">
+						{post.root_url}
+					</a>
+					<div className="date-like-heart">
+						<span className="formatted-date">
+							{moment(post.created_at).fromNow() === 'a few seconds ago' ? (
+								'just now'
+							) : (
+								<Moment fromNow>{post.created_at}</Moment>
+							)}
+						</span>
+
+						<SharedButton bookmark={post} />
+						<div className="delete-bookmark" onClick={() => props.handleDelete(post.id)}>
+							<img src={deleteIcon} className="delete-icon" alt="delete icon" />
+							<span className="del-span">Delete</span>
+						</div>
+					</div>
+					<div className="edit-icon">
+						<img
+							src={editSvg}
+							alt=""
+							onClick={() => {
+								props.handleModalOpen(post)
+							}}
+						/>
+					</div>
+				</div>
+			</Post>
+		))
+		.reverse()
+
+	return (
+		<Wrapper>
+			{props.modalOpen && (
+				<EditModal open={props.modalOpen} handleModalOpen={props.handleModalOpen} post={props.editPost} />
+			)}
+			{posts}
+		</Wrapper>
+	)
+}
+
+const Wrapper = styled.div`
+	// border: 1px solid blue;
+	${customWrapper('100%', '0 auto')};
+`
+
+const Post = styled.div`${customWrapper('100%', 'auto')} ${StyledBookmarks};`
--- src/components/collections/index.js
+++ src/components/collections/index.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import Moment from 'react-moment'
-import styled from 'styled-components'
-import { customWrapper, truncateText } from '../mixins'
-import { StyledBookmarks } from './StyledBookmarks'
-import EditModal from '../utils/EditModal/EditModal'
+
+import { truncateText } from '../mixins'
 import { getPosts, deletePost, getUserProfileDetails } from '../../actions'
-import deleteIcon from '../../assets/svg/delete-icon.svg'
-import editSvg from '../../assets/svg/edit.svg'
 import HelpScreen from '../utils/screens/HelpScreen'
 import BookmarkSVG from '../../assets/svg/bookmark-drawing.svg'
-import SharedButton from '../shared'
-import moment from 'moment'
+import Collection from './Collection'
 
 class Collections extends Component {
 	constructor(props) {
@@ -51,54 +45,15 @@ class Collections extends Component {
 			// 				? post.description.toLowerCase().indexOf(search.toLowerCase()) !== -1
 			// 				: null
 			// })
-
-			const posts = this.props.posts
-				.map((post) => (
-					<Post key={post.id}>
-						<a href={post.post_url} target="_blank" rel="noopener noreferrer">
-							<img src={post.thumbnail_url} alt="" />
-						</a>
-						<div className="post-content">
-							<a href={post.post_url} target="_blank" rel="noopener noreferrer">
-								<h1>{handleTruncateText(post.title, 9)}</h1>
-							</a>
-							<p>{handleTruncateText(post.description, 15)}</p>
-							<a className="post-root-url" href={post.post_url} target="_blank" rel="noopener noreferrer">
-								{post.root_url}
-							</a>
-							<div className="date-like-heart">
-								<span className="formatted-date">
-									{moment(post.created_at).fromNow() === 'a few seconds ago' ? (
-										'just now'
-									) : (
-										<Moment fromNow>{post.created_at}</Moment>
-									)}
-								</span>
-
-								<SharedButton bookmark={post} />
-								<div className="delete-bookmark" onClick={() => this.handleDelete(post.id)}>
-									<img src={deleteIcon} className="delete-icon" alt="delete icon" />
-									<span className="del-span">Delete</span>
-								</div>
-							</div>
-							<div className="edit-icon">
-								<img
-									src={editSvg}
-									alt=""
-									onClick={() => {
-										handleModalOpen(post)
-									}}
-								/>
-							</div>
-						</div>
-					</Post>
-				))
-				.reverse()
 			return (
-				<Wrapper>
-					{modalOpen && <EditModal open={modalOpen} handleModalOpen={handleModalOpen} post={editPost} />}
-					{posts}
-				</Wrapper>
+				<Collection
+					handleDelete={this.handleDelete}
+					handleTruncateText={this.handleTruncateText}
+					posts={this.props.posts}
+					handleModalOpen={this.handleModalOpen}
+					modalOpen={this.state.modalOpen}
+					editPost={this.state.editPost}
+				/>
 			)
 		}
 
@@ -124,10 +79,3 @@ export default connect(mapStateToProps, {
 	deletePost,
 	getUserProfileDetails
 })(Collections)
-
-export const Wrapper = styled.div`
-	// border: 1px solid blue;
-	${customWrapper('100%', '0 auto')};
-`
-
-export const Post = styled.div`${customWrapper('100%', 'auto')} ${StyledBookmarks};`
