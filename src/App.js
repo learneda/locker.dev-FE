7a5e853b38f380d5975a6c9de2c0cc4de5dabd71
cesarnml <cmejia@gmail.com>
2019-05-16 14:26:08 -0700

    code-splits routes; lazy + Suspense implementation; feedback 🦄

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, lazy, Suspense } from 'react'
 import { Route, Switch, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import styled from 'styled-components'
 import GlobalStyle from './components/mixins'
-import LandingPage from './pages/Landing'
-import Home from './pages/Home'
-import Browse from './pages/Browse'
-import Social from './pages/Social'
-import Settings from './pages/Settings'
-import NoMatch from './pages/NoMatch'
-import Profile from './pages/Profile'
-import SinglePost from './pages/SinglePost/SinglePost'
+// import LandingPage from './pages/Landing'
+// import Home from './pages/Home'
+// import Browse from './pages/Browse'
+// import Social from './pages/Social'
+// import Settings from './pages/Settings'
+// import NoMatch from './pages/NoMatch'
+// import Profile from './pages/Profile'
+// import SinglePost from './pages/SinglePost/SinglePost'
 import Navbar from './components/navigation/Navbar'
 import { customContainer } from './components/mixins'
 import { composedIndexRedirect as index } from './components/authentication/indexRedirect'
 import { composedHomeRedirect as home } from './components/authentication/homeRedirect'
 import useInterval from './components/hooks/useInterval'
 import { fetchUser, getPosts } from './actions'
-// import { ReactComponent as Loading } from './assets/svg/circles.svg';
+import { ReactComponent as Loading } from './assets/svg/circles.svg'
 //? Should we implement route-based code-splitting?
-// const LandingPage = lazy(() => import('./pages/Landing'));
-// const Home = lazy(() => import('./pages/Home'));
-// const Browse = lazy(() => import('./pages/Browse'));
-// const Social = lazy(() => import('./pages/Social'));
-// const Settings = lazy(() => import('./pages/Settings'));
-// const NoMatch = lazy(() => import('./pages/NoMatch'));
-// const Profile = lazy(() => import('./pages/Profile'));
-// const SinglePost = lazy(() => import('./pages/SinglePost/SinglePost'));
+const LandingPage = lazy(() => import('./pages/Landing'))
+const Home = lazy(() => import('./pages/Home'))
+const Browse = lazy(() => import('./pages/Browse'))
+const Social = lazy(() => import('./pages/Social'))
+const Settings = lazy(() => import('./pages/Settings'))
+const NoMatch = lazy(() => import('./pages/NoMatch'))
+const Profile = lazy(() => import('./pages/Profile'))
+const SinglePost = lazy(() => import('./pages/SinglePost/SinglePost'))
 
 const App = ({ fetchUser, modal, getPosts }) => {
   const { isAuthOpen, isEditOpen } = modal
@@ -53,7 +53,7 @@ const App = ({ fetchUser, modal, getPosts }) => {
     <Container>
       <GlobalStyle />
       <Navbar />
-      {/* <Suspense
+      <Suspense
         fallback={
           <div
             style={{
@@ -65,18 +65,18 @@ const App = ({ fetchUser, modal, getPosts }) => {
             <Loading />
           </div>
         }
-      > */}
-      <Switch>
-        <Route component={home(LandingPage)} path='/' exact />
-        <Route component={index(Home)} path='/home' />
-        <Route component={index(Browse)} path='/browse' />
-        <Route component={index(Social)} path='/social' />
-        <Route component={index(Profile)} path='/profile/:id' />
-        <Route component={index(SinglePost)} path='/status/:id' />
-        <Route component={index(Settings)} path='/settings' />
-        <Route component={NoMatch} />
-      </Switch>
-      {/* </Suspense> */}
+      >
+        <Switch>
+          <Route component={home(LandingPage)} path='/' exact />
+          <Route component={index(Home)} path='/home' />
+          <Route component={index(Browse)} path='/browse' />
+          <Route component={index(Social)} path='/social' />
+          <Route component={index(Profile)} path='/profile/:id' />
+          <Route component={index(SinglePost)} path='/status/:id' />
+          <Route component={index(Settings)} path='/settings' />
+          <Route component={NoMatch} />
+        </Switch>
+      </Suspense>
     </Container>
   )
 }
