7a165ad92907decd733ab48cb4208801c687fb44
Riley-Brown <riley@riley.gg>
2019-05-17 23:31:58 -0400

    add some styles to notification nav

--- src/components/navigation/Notifications.js
+++ src/components/navigation/Notifications.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import ModalPortal from '../utils/ModalPortal'
 import { Link } from 'react-router-dom'
 import { readNotifications, deleteNotifications } from '../../actions'
+import styled from 'styled-components'
 class Notifications extends Component {
   constructor() {
     super()
@@ -15,60 +16,66 @@ class Notifications extends Component {
     let count = this.props.notifications
     if (count.length === 0) {
       return (
-        <div
-          onClick={() => {
-            this.setState({ toggle: !this.state.toggle })
-            this.props.readNotifications()
-          }}
-        >
-          <img src={bellIcon} alt='bell-icon' className='bell-icon' />
-          {count}
-          {this.state.toggle && (
-            <ModalPortal>
-              <h1
-                onClick={() => {
-                  this.setState({ toggle: !this.state.toggle })
-                  this.props.deleteNotifications()
-                }}
-              >
-                clear
-              </h1>
-              <div>YOU have no notification</div>
-            </ModalPortal>
-          )}
-        </div>
+        <Link to='/notifications'>
+          <StyledNotifications
+            onClick={() => {
+              // this.setState({ toggle: !this.state.toggle })
+              this.props.readNotifications()
+            }}
+          >
+            <span id='notifications'>Notifications</span>
+            <img src={bellIcon} alt='bell-icon' className='bell-icon' />
+            {count}
+            {this.state.toggle && (
+              <ModalPortal>
+                <h1
+                  onClick={() => {
+                    this.setState({ toggle: !this.state.toggle })
+                    this.props.deleteNotifications()
+                  }}
+                >
+                  clear
+                </h1>
+                <div>YOU have no notification</div>
+              </ModalPortal>
+            )}
+          </StyledNotifications>
+        </Link>
       )
     } else {
       count = count.length
       return (
-        <div
-          onClick={() => {
-            this.setState({ toggle: !this.state.toggle })
-            this.props.readNotifications()
-          }}
-        >
-          <img src={bellIcon} alt='bell-icon' className='bell-icon' />
-          {count}
-          {this.state.toggle && (
-            <ModalPortal>
-              <h1
-                onClick={() => {
-                  this.setState({ toggle: !this.state.toggle })
-                  this.props.deleteNotifications()
-                }}
-              >
-                clear
-              </h1>
-              {this.props.notifications.map((obj, i) => {
-                return (
-                  <Link to={`/status/${obj.post_id}`}>
-                    <div>{`${obj.invoker} ${obj.type} on your post`}</div>
-                  </Link>
-                )
-              })}
-            </ModalPortal>
-          )}
-        </div>
+        <Link to='/notifications'>
+          <StyledNotifications
+            onClick={() => {
+              this.setState({ toggle: !this.state.toggle })
+              this.props.readNotifications()
+            }}
+          >
+            <span id='notifications'>Notifications</span>
+            <img src={bellIcon} alt='bell-icon' className='bell-icon' />
+            <p>{count}</p>
+            {this.state.toggle && (
+              <ModalPortal>
+                <h1
+                  onClick={() => {
+                    this.setState({ toggle: !this.state.toggle })
+                    this.props.deleteNotifications()
+                  }}
+                >
+                  clear
+                </h1>
+                {this.props.notifications.map((obj, i) => {
+                  return (
+                    <Link to={`/status/${obj.post_id}`}>
+                      <div>{`${obj.invoker} ${obj.type} on your post`}</div>
+                    </Link>
+                  )
+                })}
+              </ModalPortal>
+            )}
+          </StyledNotifications>
+        </Link>
       )
     }
   }
@@ -80,3 +87,27 @@ export default connect(
   mapStateToProps,
   { readNotifications, deleteNotifications }
 )(Notifications)
+
+const StyledNotifications = styled.div`
+  display: flex;
+  padding: 5px 10px;
+  border-radius: 5px;
+  /* background: #f4511e;
+  color: #fff; */
+  color: #333;
+  #notifications {
+    background: none;
+    margin: 0;
+    padding: 5px 2px;
+    color: #333;
+  }
+  span {
+    background: none;
+    margin: 0;
+    padding: 0;
+  }
+  p {
+    /* align-self: flex-end; */
+    color: #3f65f2;
+  }
+`
