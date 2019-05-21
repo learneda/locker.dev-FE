21f611ba0502ec4c6f2bf2029bc9a06a92c5bfa8
Riley-Brown <riley@riley.gg>
2019-05-20 23:25:27 -0400

    removed grommet and fixed dot hell on imports

--- src/pages/Browse/index.js
+++ src/pages/Browse/index.js
@@ -3,13 +3,12 @@ import { connect } from 'react-redux'
 import { NavLink, Route, Switch, withRouter } from 'react-router-dom'
 import { withAlert } from 'react-alert'
 import axios from 'axios'
-import { Grommet } from 'grommet'
 import styled from 'styled-components'
-import Courses from '../../components/browse/Courses'
-import Videos from '../../components/browse/Videos'
-import Articles from '../../components/browse/Articles'
-import Podcasts from '../../components/browse/Podcasts'
-import Books from '../../components/browse/Books'
+import Courses from 'components/browse/Courses'
+import Videos from 'components/browse/Videos'
+import Articles from 'components/browse/Articles'
+import Podcasts from 'components/browse/Podcasts'
+import Books from 'components/browse/Books'
 import {
   fetchUser,
   fetchCourses,
@@ -82,101 +81,99 @@ class Browse extends Component {
       match,
     } = this.props
     return (
-      <Grommet theme={theme}>
-        <Wrapper>
-          <BrowseContainer>
-            <Tabs>
-              <Tab>
-                <NavLink exact to={`${match.url}/courses`}>
-                  Course
-                </NavLink>
-              </Tab>
-              <Tab>
-                <NavLink to={`${match.url}/articles`}>Article</NavLink>
-              </Tab>
-              <Tab>
-                <NavLink to={`${match.url}/videos`}>Video</NavLink>
-              </Tab>
-              <Tab>
-                <NavLink to={`${match.url}/books`}>Book</NavLink>
-              </Tab>
-              <Tab>
-                <NavLink to={`${match.url}/podcasts`}>Podcast</NavLink>
-              </Tab>
-            </Tabs>
-            <TabWrapper>
-              <Switch>
-                <Route
-                  exact
-                  path={[`${match.path}`, `${match.path}/courses`]}
-                  render={props => (
-                    <Courses
-                      {...props}
-                      courses={courses}
-                      searchTerm={searchTerm}
-                      coursePage={coursePage}
-                      setCoursePage={setCoursePage}
-                      searchCourses={searchCourses}
-                      fetchMoreCourses={this.fetchMoreCourses}
-                      handleSaveLink={this.handleSaveLink}
-                      handleTruncateText={this.handleTruncateText}
-                      alert={this.props.alert}
-                    />
-                  )}
-                />
-                <Route
-                  path={`${match.path}/articles`}
-                  render={props => (
-                    <Articles
-                      {...props}
-                      articles={articles}
-                      searchTerm={searchTerm}
-                      articleOffset={articleOffset}
-                      setArticleOffset={setArticleOffset}
-                      searchArticles={searchArticles}
-                      fetchMoreArticles={this.fetchMoreArticles}
-                      handleSaveLink={this.handleSaveLink}
-                      handleTruncateText={this.handleTruncateText}
-                      alert={this.props.alert}
-                    />
-                  )}
-                />
-                <Route
-                  path={`${match.path}/videos`}
-                  render={props => (
-                    <Videos
-                      {...props}
-                      handleTruncateText={this.handleTruncateText}
-                      handleSaveMedia={this.handleSaveMedia}
-                      alert={this.props.alert}
-                    />
-                  )}
-                />
-                <Route
-                  path={`${match.path}/books`}
-                  render={props => (
-                    <Books
-                      {...props}
-                      handleSaveMedia={this.handleSaveMedia}
-                      alert={this.props.alert}
-                    />
-                  )}
-                />
-                <Route
-                  path={`${match.path}/podcasts`}
-                  render={props => (
-                    <Podcasts
-                      {...props}
-                      handleSaveMedia={this.handleSaveMedia}
-                      alert={this.props.alert}
-                    />
-                  )}
-                />
-              </Switch>
-            </TabWrapper>
-          </BrowseContainer>
-        </Wrapper>
-      </Grommet>
+      <Wrapper>
+        <BrowseContainer>
+          <Tabs>
+            <Tab>
+              <NavLink exact to={`${match.url}/courses`}>
+                Course
+              </NavLink>
+            </Tab>
+            <Tab>
+              <NavLink to={`${match.url}/articles`}>Article</NavLink>
+            </Tab>
+            <Tab>
+              <NavLink to={`${match.url}/videos`}>Video</NavLink>
+            </Tab>
+            <Tab>
+              <NavLink to={`${match.url}/books`}>Book</NavLink>
+            </Tab>
+            <Tab>
+              <NavLink to={`${match.url}/podcasts`}>Podcast</NavLink>
+            </Tab>
+          </Tabs>
+          <TabWrapper>
+            <Switch>
+              <Route
+                exact
+                path={[`${match.path}`, `${match.path}/courses`]}
+                render={props => (
+                  <Courses
+                    {...props}
+                    courses={courses}
+                    searchTerm={searchTerm}
+                    coursePage={coursePage}
+                    setCoursePage={setCoursePage}
+                    searchCourses={searchCourses}
+                    fetchMoreCourses={this.fetchMoreCourses}
+                    handleSaveLink={this.handleSaveLink}
+                    handleTruncateText={this.handleTruncateText}
+                    alert={this.props.alert}
+                  />
+                )}
+              />
+              <Route
+                path={`${match.path}/articles`}
+                render={props => (
+                  <Articles
+                    {...props}
+                    articles={articles}
+                    searchTerm={searchTerm}
+                    articleOffset={articleOffset}
+                    setArticleOffset={setArticleOffset}
+                    searchArticles={searchArticles}
+                    fetchMoreArticles={this.fetchMoreArticles}
+                    handleSaveLink={this.handleSaveLink}
+                    handleTruncateText={this.handleTruncateText}
+                    alert={this.props.alert}
+                  />
+                )}
+              />
+              <Route
+                path={`${match.path}/videos`}
+                render={props => (
+                  <Videos
+                    {...props}
+                    handleTruncateText={this.handleTruncateText}
+                    handleSaveMedia={this.handleSaveMedia}
+                    alert={this.props.alert}
+                  />
+                )}
+              />
+              <Route
+                path={`${match.path}/books`}
+                render={props => (
+                  <Books
+                    {...props}
+                    handleSaveMedia={this.handleSaveMedia}
+                    alert={this.props.alert}
+                  />
+                )}
+              />
+              <Route
+                path={`${match.path}/podcasts`}
+                render={props => (
+                  <Podcasts
+                    {...props}
+                    handleSaveMedia={this.handleSaveMedia}
+                    alert={this.props.alert}
+                  />
+                )}
+              />
+            </Switch>
+          </TabWrapper>
+        </BrowseContainer>
+      </Wrapper>
     )
   }
 }
@@ -201,36 +198,6 @@ export default connect(
     createPost,
   }
 )(withRouter(Alert))
-
-const theme = {
-  tab: {
-    color: 'dark-1',
-    active: {
-      weight: 'bold',
-    },
-    border: {
-      side: 'bottom',
-      size: 'medium',
-      color: {
-        light: null,
-      },
-      active: {
-        color: {
-          light: 'dark-1',
-        },
-      },
-      hover: {
-        color: {
-          light: null,
-        },
-      },
-    },
-    margin: {
-      vertical: 'small',
-      horizontal: 'xsmall',
-    },
-  },
-}
 
 const BrowseContainer = styled.div`
   h2 {
