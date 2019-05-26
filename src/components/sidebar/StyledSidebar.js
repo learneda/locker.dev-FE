48df7ea452932cfe5db6c736e0776dcc870c18dc
Riley-Brown <riley@riley.gg>
2019-05-25 22:57:28 -0400

    made sidebar position fixed to fix weird bug

--- src/components/sidebar/StyledSidebar.js
+++ src/components/sidebar/StyledSidebar.js
@@ -2,8 +2,10 @@ export const StyledSidebar = () => `
 box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
 border-radius: 5px;
 background: #fff;
-position: sticky;
+position: fixed;
 top: 105px;
+width: 300px;
+z-index: 200;
 
 .user {
   border-top-right-radius: 5px;
