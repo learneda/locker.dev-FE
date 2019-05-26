ed7e90b78f5125aac8454f43c6af314696734d64
Riley-Brown <riley@riley.gg>
2019-05-26 12:46:51 -0400

    fixed sidebar not loading

--- src/pages/Notifications/index.js
+++ src/pages/Notifications/index.js
@@ -3,17 +3,52 @@ import Sidebar from 'components/sidebar/Sidebar'
 import { customWrapper } from 'components/mixins'
 import styled from 'styled-components'
 import Notifications from 'components/notifications'
+import { connect } from 'react-redux'
+import { fetchUser } from 'actions'
 
-export default class index extends Component {
+class Notification extends Component {
+  componentDidMount() {
+    this.props.fetchUser(this.props.auth.id)
+  }
   render() {
+    const {
+      auth,
+      user,
+      collections,
+      followers,
+      following,
+      fetchFollowers,
+      fetchFollowing,
+    } = this.props
     return (
       <Container>
-        <Sidebar />
+        <Sidebar
+          auth={auth}
+          user={user}
+          collections={collections}
+          followers={followers}
+          following={following}
+          fetchFollowers={fetchFollowers}
+          fetchFollowing={fetchFollowing}
+        />
         <Notifications />
       </Container>
     )
   }
 }
+const mapStateToProps = ({ auth, user, collections, social }) => ({
+  auth,
+  user,
+  collections,
+  ...social,
+})
+
+export default connect(
+  mapStateToProps,
+  {
+    fetchUser,
+  }
+)(Notification)
 
 const Container = styled.div`
   ${customWrapper('80%', '0 auto')} display: flex;
