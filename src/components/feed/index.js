bf13faa89844bfed472438fb42fed24b16cfa20f
cesarnml <cmejia@gmail.com>
2019-05-26 16:20:52 -0700

    hotfix sidepanel back to sticky; sticky all things 💩

--- src/components/feed/index.js
+++ src/components/feed/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
 import openSocket from 'socket.io-client'
 import styled from 'styled-components'
 import { customWrapper } from '../mixins'
