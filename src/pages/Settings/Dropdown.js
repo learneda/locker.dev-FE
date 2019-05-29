8995d98312556e415e97a540daefb2a775a95c6f
luis <mrluismartinezzz@gmail.com>
2019-05-28 20:10:05 -0700

    react-dropzone is now a dependency

---
+++ src/pages/Settings/Dropdown.js
@@ -0,0 +1,23 @@
+import React, { useCallback } from 'react'
+import { useDropzone } from 'react-dropzone'
+
+function MyDropzone() {
+  const onDrop = useCallback(acceptedFiles => {
+    console.log(acceptedFiles)
+    // Do something with the files
+  }, [])
+  const { getRootProps, getInputProps } = useDropzone({ onDrop })
+
+  return (
+    <div style={{ border: '2px solid red' }} {...getRootProps()}>
+      <input {...getInputProps()} />
+      {true ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      )}
+    </div>
+  )
+}
+
+export default MyDropzone
