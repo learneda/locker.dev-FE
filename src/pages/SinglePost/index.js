5da6ae40bf774cee922d58a5eb02ca45134e052b
Riley-Brown <riley@riley.gg>
2019-06-02 23:36:49 -0400

    imported moment and styled post header

--- src/pages/SinglePost/index.js
+++ src/pages/SinglePost/index.js
@@ -3,6 +3,8 @@ import axios from 'apis/axiosAPI'
 import styled from 'styled-components'
 import { withRouter } from 'react-router-dom'
 import PostContainer from 'components/feed/posts'
+import { StyledFeed } from 'components/feed/StyledFeed'
+import Moment from 'react-moment'
 
 const SinglePost = props => {
   const [post, setPost] = useState('')
@@ -16,11 +18,13 @@ const SinglePost = props => {
       <div className='post-header'>
         <img className='profile_pic' src={post.profile_picture} />
         <div className='post-info'>
-          <div>
-            <span>{post.username}</span>
-            <span>{post.created_at}</span>
+          <div className='post-author'>
+            <h2>{post.username}</h2>
+            <span>
+              <Moment fromNow>{post.created_at}</Moment>
+            </span>
           </div>
-          <div>{post.user_thoughts}</div>
+          <div className='post-thoughts'>{post.user_thoughts}</div>
         </div>
       </div>
       <div className='post-image'>
@@ -64,43 +68,71 @@ const SinglePost = props => {
 export default withRouter(SinglePost)
 
 const Container = styled.div`
-  border: 1px solid red;
-  margin: 0 auto;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  margin: 0 1px 40px;
+  border-radius: 8px;
+  background: #fff;
+  margin: 40px auto;
   max-width: 600px;
   padding: 30px;
   display: flex;
   flex-direction: column;
   .post-header {
-    outline: 1px solid blue;
     display: flex;
     padding: 5px;
+    display: flex;
+    align-items: center;
     border-bottom: 1px solid lightgray;
+    padding-bottom: 15px;
     .profile_pic {
-      height: 50px;
-      width: 50px;
+      height: 60px;
+      width: 60px;
       border-radius: 50%;
+      margin-right: 15px;
     }
     .post-info {
-      outline: 1px solid red;
       display: flex;
       flex-direction: column;
       justify-content: space-around;
+      .post-author {
+        display: flex;
+        align-items: baseline;
+        h2 {
+          font-size: 1.5rem;
+          font-weight: 500;
+          margin-bottom: 2px;
+        }
+        span {
+          font-size: 1.4rem;
+          opacity: 0.8;
+          font-weight: normal;
+          margin-left: 7px;
+          &::before {
+            content: '-';
+            margin-right: 5px;
+          }
+        }
+      }
+      .post-thoughts {
+        font-size: 2rem;
+        font-weight: 400;
+      }
     }
   }
   .post-image {
-    outline: 1px solid orange;
+    max-height: 500px;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
   .post-content {
-    outline: 1px solid green;
     h2 {
-    font-size: 2rem;
-    font-weight: bold;
+      font-size: 2rem;
+      font-weight: bold;
     }
   }
   .post-actionbar {
-    outline: 1px solid purple;
   }
   .post-comments {
-    outline 1px solid salmon;
   }
 `
