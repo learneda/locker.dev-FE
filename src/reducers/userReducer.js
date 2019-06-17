cf6c265229ec080ad286ce47c78f9afe6eeb3126
luis <mrluismartinezzz@gmail.com>
2019-06-17 15:09:15 -0700

    created action types 4 updating pic

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,11 +1,19 @@
 import { EDIT_USER } from 'actions/types'
-import { FETCH_USER } from 'appTypes'
+import {
+  FETCH_USER,
+  UPDATE_HEADER_PICTURE,
+  UPDATE_PROFILE_PICTURE,
+} from 'appTypes'
 export const userReducer = (state = null, action) => {
   switch (action.type) {
     case FETCH_USER:
       return action.payload
     case EDIT_USER:
       return action.payload
+    case UPDATE_HEADER_PICTURE:
+      return { ...state, header_picture: action.payload }
+    case UPDATE_PROFILE_PICTURE:
+      return { ...state, profile_picture: action.payload }
     default:
       return state
   }
