e74501412f8c522b787645d9b49dc77018769032
luis <mrluismartinezzz@gmail.com>
2019-06-17 22:29:36 -0700

    fixed if condition

--- src/pages/HashTag/tagActions.js
+++ src/pages/HashTag/tagActions.js
@@ -3,7 +3,7 @@ import axios from 'apis/axiosAPI'
 
 export const fetchTagPosts = hashtag => async dispatch => {
   const posts = await axios.get(`/tags/${hashtag}`)
-  if (posts.data.length) {
+  if (posts.data.response) {
     console.log('fetchTagPosts response', posts)
     dispatch({ type: type.FETCH_TAG_POSTS, payload: posts.data.response })
   }
