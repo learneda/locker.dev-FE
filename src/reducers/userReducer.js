ad2e436acf4c433f71eca2f7ddcebe3d0f5ca208
Luis Martinez <mrluismartinezzz@gmail.com>
2022-02-25 11:21:04 -0800

    logRocket identifying session

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -4,9 +4,16 @@ import {
   UPDATE_HEADER_PICTURE,
   UPDATE_PROFILE_PICTURE,
 } from 'App/store/appTypes'
+import LogRocket from 'logrocket'
+
 export const userReducer = (state = {}, action) => {
   switch (action.type) {
     case FETCH_USER:
+      if (process.env.NODE_ENV === 'production') {
+        LogRocket.identify(JSON.stringify(action.payload.id), {
+          username: action.payload.username,
+        })
+      }
       return action.payload
     case EDIT_USER:
       return action.payload
