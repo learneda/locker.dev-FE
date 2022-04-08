8702909a59c23fdc7fe7ad961d4897aad6f9fcc0
Luis Martinez <mrluismartinezzz@gmail.com>
2022-04-08 11:25:02 -0700

    Create timestampUpdatedLocale.js
    making it dry.

---
+++ src/components/Utils/momentjs/timestampUpdatedLocale.js
@@ -0,0 +1,20 @@
+export function updateMomentLocale(m) {
+  return m.updateLocale('en', {
+    relativeTime: {
+      future: 'in %s',
+      past: '%s ago',
+      s: 'a few seconds',
+      ss: '%d seconds',
+      m: 'a minute',
+      mm: '%d minutes',
+      h: 'an hour',
+      hh: '%d hours',
+      d: 'a day',
+      dd: '%d days',
+      M: 'a month',
+      MM: '%d months',
+      y: 'a year',
+      yy: '%d years',
+    },
+  })
+}
